e1b832b2904050c688388a3c9d371561b0015906
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-23 21:46:45 +0100

    build(deps): add jakarta validation and Hibernate Validator dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,8 @@ dependencies {
 	"developmentOnly"("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
 	runtimeOnly("org.postgresql:postgresql")
+	implementation("jakarta.validation:jakarta.validation-api:3.0.2")
+	implementation("org.hibernate.validator:hibernate-validator:8.0.1.Final")
 }
 
 tasks.withType<Test> {
