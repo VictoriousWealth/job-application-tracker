672bd759ecd103aec024f0a58740ccf21c1b143d
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-23 21:00:00 +0100

    refactor(dto): revise AttachmentDTO fields and add constructor for consistency

--- src/main/java/com/nick/job_application_tracker/dto/AttachmentDTO.java
+++ src/main/java/com/nick/job_application_tracker/dto/AttachmentDTO.java
@@ -1,6 +1,17 @@
+package com.nick.job_application_tracker.dto;
+
 public class AttachmentDTO {
     public Long id;
-    public String fileName;
-    public String fileType;
-    public String uploadDate;
+    public String type;
+    public String filePath;
+    public Long jobApplicationId;
+
+    public AttachmentDTO() {}
+
+    public AttachmentDTO(Long id, String type, String filePath, Long jobApplicationId) {
+        this.id = id;
+        this.type = type;
+        this.filePath = filePath;
+        this.jobApplicationId = jobApplicationId;
+    }
 }
