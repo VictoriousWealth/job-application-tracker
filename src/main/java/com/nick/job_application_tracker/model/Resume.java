5d96bdca8f3df2f46681fccdc0f9074fc42a1980
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-23 06:33:44 +0100

    Add Resume entity

---
+++ src/main/java/com/nick/job_application_tracker/model/Resume.java
@@ -0,0 +1,33 @@
+package com.nick.job_application_tracker.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "resume")
+public class Resume {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "file_path", nullable = false)
+    private String filePath;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
