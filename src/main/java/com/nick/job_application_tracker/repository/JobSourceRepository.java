001d6b6c5f8e0e7113e62366d9f8ab32d5b63b4a
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-23 17:57:31 +0100

    Add JobSourceRepository

---
+++ src/main/java/com/nick/job_application_tracker/repository/JobSourceRepository.java
@@ -0,0 +1,10 @@
+package com.nick.job_application_tracker.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nick.job_application_tracker.model.JobSource;
+
+@Repository
+public interface JobSourceRepository extends JpaRepository<JobSource, Long> {
+}
