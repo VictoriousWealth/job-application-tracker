9cda6124a002f3cb41c84527275680ac3aa6b87e
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-23 19:07:46 +0100

    feat(controller): add CommunicationLogController for managing communication logs

---
+++ src/main/java/com/nick/job_application_tracker/controller/CommunicationLogController.java
@@ -0,0 +1,42 @@
+package com.nick.job_application_tracker.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nick.job_application_tracker.model.CommunicationLog;
+import com.nick.job_application_tracker.service.CommunicationLogService;
+
+@RestController
+@RequestMapping("/api/communications")
+public class CommunicationLogController {
+
+    private final CommunicationLogService service;
+
+    public CommunicationLogController(CommunicationLogService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/job/{jobAppId}")
+    public List<CommunicationLog> getForJob(@PathVariable Long jobAppId) {
+        return service.getByJobAppId(jobAppId);
+    }
+
+    @PostMapping
+    public CommunicationLog create(@RequestBody CommunicationLog log) {
+        return service.save(log);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
