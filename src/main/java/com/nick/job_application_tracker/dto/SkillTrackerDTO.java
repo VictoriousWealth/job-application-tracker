1ea7a664b598cd55e90a5ba028f241f995413e32
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-23 20:48:09 +0100

    feat(dto): add SkillTrackerDTO

---
+++ src/main/java/com/nick/job_application_tracker/dto/SkillTrackerDTO.java
@@ -0,0 +1,6 @@
+public class SkillTrackerDTO {
+    public Long id;
+    public String skillName;
+    public String proficiency; // e.g., BEGINNER, INTERMEDIATE, EXPERT
+    public Long jobApplicationId;
+}
