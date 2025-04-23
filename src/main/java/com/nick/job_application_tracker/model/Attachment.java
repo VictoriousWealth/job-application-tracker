74dce40dd008b15db1a910a37072efd0da6374c0
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-23 06:33:44 +0100

    Add Attachment entity

---
+++ src/main/java/com/nick/job_application_tracker/model/Attachment.java
@@ -0,0 +1,69 @@
+package com.nick.job_application_tracker.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "attachment")
+public class Attachment {
+
+    public enum Type {
+        JOB_DESCRIPTION,
+        OFFER_LETTER,
+        INTERVIEW_PREP,
+        REJECTION_LETTER,
+        OTHER
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Type type;
+
+    @Column(name = "file_path", nullable = false)
+    private String filePath;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "job_application_id")
+    private JobApplication jobApplication;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public JobApplication getJobApplication() {
+        return jobApplication;
+    }
+
+    public void setJobApplication(JobApplication jobApplication) {
+        this.jobApplication = jobApplication;
+    }
+}
