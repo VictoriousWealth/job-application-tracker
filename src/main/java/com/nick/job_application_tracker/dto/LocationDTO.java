52fe1706a3c88a85714e7f917f9f37480d9d0b4f
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-23 23:24:46 +0100

    feat(dto): add LocationDTO

--- src/main/java/com/nick/job_application_tracker/dto/LocationDTO.java
+++ src/main/java/com/nick/job_application_tracker/dto/LocationDTO.java
@@ -1,13 +1,41 @@
 package com.nick.job_application_tracker.dto;
 
 public class LocationDTO {
-    public Long id;
-    public String city;
-    public String country;
+    private Long id;
+    private String city;
+    private String country;
+
+    // Constructors
+    public LocationDTO() {}
 
     public LocationDTO(Long id, String city, String country) {
         this.id = id;
         this.city = city;
+        this.country = country;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
         this.country = country;
     }
 }
