c3fbf7283d1407717939d86c5c64d042419f664a
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-23 21:46:45 +0100

    feat(security): add password encoder bean for secure password hashing

--- src/main/java/com/nick/job_application_tracker/config/SecurityConfig.java
+++ src/main/java/com/nick/job_application_tracker/config/SecurityConfig.java
@@ -12,6 +12,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.context.request.async.WebAsyncManagerIntegrationFilter;
@@ -72,6 +74,11 @@ public class SecurityConfig {
     @Bean
     public RequestMatcher requestMatcher() {
         return new AntPathRequestMatcher("**");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
