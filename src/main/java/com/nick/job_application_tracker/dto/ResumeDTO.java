eb2f4d92e56e40bad675197537acb37e1919b6ca
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-23 23:24:46 +0100

    feat(dto): add ResumeDTO

--- src/main/java/com/nick/job_application_tracker/dto/ResumeDTO.java
+++ src/main/java/com/nick/job_application_tracker/dto/ResumeDTO.java
@@ -1,11 +1,29 @@
 package com.nick.job_application_tracker.dto;
 
 public class ResumeDTO {
-    public Long id;
-    public String filePath;
+    private Long id;
+    private String filePath;
+
+    public ResumeDTO() {}
 
     public ResumeDTO(Long id, String filePath) {
         this.id = id;
+        this.filePath = filePath;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFilePath(String filePath) {
         this.filePath = filePath;
     }
 }
