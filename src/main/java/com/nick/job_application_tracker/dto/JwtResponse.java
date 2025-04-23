048f9db35eeb202969bdfa05aedcb0a99dd92ad8
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-23 19:07:46 +0100

    feat(dto): add JwtResponse to return token after login

---
+++ src/main/java/com/nick/job_application_tracker/dto/JwtResponse.java
@@ -0,0 +1,9 @@
+package com.nick.job_application_tracker.dto;
+
+public class JwtResponse {
+    public String token;
+
+    public JwtResponse(String token) {
+        this.token = token;
+    }
+}
