27fc9b227f930eaabe9e66fca8fe16614211df94
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-23 23:24:46 +0100

    feat(mapper): create ResumeMapper for entity <-> DTO conversion

---
+++ src/main/java/com/nick/job_application_tracker/mapper/ResumeMapper.java
@@ -0,0 +1,16 @@
+package com.nick.job_application_tracker.mapper;
+
+import com.nick.job_application_tracker.dto.ResumeDTO;
+import com.nick.job_application_tracker.model.Resume;
+
+public class ResumeMapper {
+    public static ResumeDTO toDTO(Resume resume) {
+        return new ResumeDTO(resume.getId(), resume.getFilePath());
+    }
+
+    public static Resume toEntity(ResumeDTO dto) {
+        Resume resume = new Resume();
+        resume.setFilePath(dto.getFilePath());
+        return resume;
+    }
+}
