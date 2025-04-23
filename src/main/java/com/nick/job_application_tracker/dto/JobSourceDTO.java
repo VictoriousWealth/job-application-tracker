80cc9280799614d0478a4e6f41ff132f452a6996
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-23 20:48:08 +0100

    feat(dto): add JobSourceDTO

---
+++ src/main/java/com/nick/job_application_tracker/dto/JobSourceDTO.java
@@ -0,0 +1,11 @@
+package com.nick.job_application_tracker.dto;
+
+public class JobSourceDTO {
+    public Long id;
+    public String name;
+
+    public JobSourceDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
