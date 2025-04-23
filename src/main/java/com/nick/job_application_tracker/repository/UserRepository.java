7122c692201eb915ac54ba5d3da168180fd8cbf5
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-23 03:09:31 +0100

    Add UserRepository for user DB operations

---
+++ src/main/java/com/nick/job_application_tracker/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.nick.job_application_tracker.repository;
+
+import com.nick.job_application_tracker.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
