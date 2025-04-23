5643f7789bdc7b76312d95a70b8d448bcfa621d4
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-23 19:07:46 +0100

    feat(controller): add AttachmentController for file-based attachments

---
+++ src/main/java/com/nick/job_application_tracker/controller/AttachmentController.java
@@ -0,0 +1,43 @@
+package com.nick.job_application_tracker.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nick.job_application_tracker.model.Attachment;
+import com.nick.job_application_tracker.service.AttachmentService;
+
+@RestController
+@RequestMapping("/api/attachments")
+public class AttachmentController {
+
+    private final AttachmentService service;
+
+    public AttachmentController(AttachmentService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/job/{jobAppId}")
+    public List<Attachment> getForJob(@PathVariable Long jobAppId) {
+        return service.getByJobAppId(jobAppId);
+    }
+
+    @PostMapping
+    public Attachment create(@RequestBody Attachment attachment) {
+        return service.save(attachment);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
