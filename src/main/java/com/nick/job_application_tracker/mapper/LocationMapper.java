1c33ca95c5a672684ca2ad02cac107cc4827499d
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-23 23:24:46 +0100

    feat(mapper): add LocationMapper for entity <-> DTO conversion

---
+++ src/main/java/com/nick/job_application_tracker/mapper/LocationMapper.java
@@ -0,0 +1,25 @@
+package com.nick.job_application_tracker.mapper;
+
+import com.nick.job_application_tracker.dto.LocationDTO;
+import com.nick.job_application_tracker.model.Location;
+
+public class LocationMapper {
+
+    public static LocationDTO toDTO(Location location) {
+        if (location == null) return null;
+        return new LocationDTO(location.getId(), location.getCity(), location.getCountry());
+    }
+
+    public static Location toEntity(LocationDTO dto) {
+        if (dto == null) return null;
+        Location location = new Location();
+        location.setCity(dto.getCity());
+        location.setCountry(dto.getCountry());
+        return location;
+    }
+
+    public static void updateEntity(Location location, LocationDTO dto) {
+        if (dto.getCity() != null) location.setCity(dto.getCity());
+        if (dto.getCountry() != null) location.setCountry(dto.getCountry());
+    }
+}
