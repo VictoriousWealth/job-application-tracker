d8c20cf2a457189f2faf04d30ddcdd939792a802
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-23 06:33:44 +0100

    Add CoverLetter entity

---
+++ src/main/java/com/nick/job_application_tracker/model/CoverLetter.java
@@ -0,0 +1,56 @@
+package com.nick.job_application_tracker.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cover_letter")
+public class CoverLetter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(name = "file_path")
+    private String filePath;
+
+    @Lob
+    private String content;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
