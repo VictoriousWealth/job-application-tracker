353e0f427d15d31a9a03f93fbc342d3dcce38643
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-23 22:50:56 +0100

    refactor(service): use CoverLetterDTO in CoverLetterService

--- src/main/java/com/nick/job_application_tracker/service/CoverLetterService.java
+++ src/main/java/com/nick/job_application_tracker/service/CoverLetterService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.nick.job_application_tracker.dto.CoverLetterDTO;
 import com.nick.job_application_tracker.model.CoverLetter;
 import com.nick.job_application_tracker.repository.CoverLetterRepository;
 
@@ -18,15 +19,24 @@ public class CoverLetterService {
         this.coverLetterRepository = coverLetterRepository;
     }
 
-    public List<CoverLetter> findAll() {
-        return coverLetterRepository.findAll();
+    public List<CoverLetterDTO> findAll() {
+        return coverLetterRepository.findAll().stream()
+            .map(cl -> new CoverLetterDTO(cl.getId(), cl.getTitle(), cl.getFilePath(), cl.getContent()))
+            .toList();
     }
 
-    public CoverLetter save(CoverLetter coverLetter) {
-        return coverLetterRepository.save(coverLetter);
-    }
+    public CoverLetterDTO save(CoverLetterDTO dto) {
+        CoverLetter coverLetter = new CoverLetter();
+        coverLetter.setTitle(dto.getTitle());
+        coverLetter.setFilePath(dto.getFilePath());
+        coverLetter.setContent(dto.getContent());
 
+        CoverLetter saved = coverLetterRepository.save(coverLetter);
+        return new CoverLetterDTO(saved.getId(), saved.getTitle(), saved.getFilePath(), saved.getContent());
+    }
+    
     public void delete(Long id) {
         coverLetterRepository.deleteById(id);
     }
+
 }
