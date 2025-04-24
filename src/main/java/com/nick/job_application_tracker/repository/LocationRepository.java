aee995a08a9a582d1e1b8a244dfbb2b4a6de421f
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-24 17:16:14 +0100

    feat(repo): add findByCityAndCountry method in LocationRepository

--- src/main/java/com/nick/job_application_tracker/repository/LocationRepository.java
+++ src/main/java/com/nick/job_application_tracker/repository/LocationRepository.java
@@ -1,10 +1,11 @@
 package com.nick.job_application_tracker.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.nick.job_application_tracker.model.Location;
 
-@Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
+    Optional<Location> findByCityAndCountry(String city, String country);
 }
