03f3288f5ed510c577910adfce78af95a54c9507
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-24 19:08:15 +0100

    feat(dto): add getters and setters to ApplicationTimelineDTO

--- src/main/java/com/nick/job_application_tracker/dto/ApplicationTimelineDTO.java
+++ src/main/java/com/nick/job_application_tracker/dto/ApplicationTimelineDTO.java
@@ -18,4 +18,46 @@ public class ApplicationTimelineDTO {
         this.description = description;
         this.jobApplicationId = jobApplicationId;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public LocalDateTime getEventTime() {
+        return eventTime;
+    }
+
+    public void setEventTime(LocalDateTime eventTime) {
+        this.eventTime = eventTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getJobApplicationId() {
+        return jobApplicationId;
+    }
+
+    public void setJobApplicationId(Long jobApplicationId) {
+        this.jobApplicationId = jobApplicationId;
+    }
+
+    
 }
