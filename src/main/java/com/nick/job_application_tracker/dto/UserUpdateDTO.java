94da2756a8a1f096d365480a2e53e8af3f6c9a01
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-24 19:08:15 +0100

    feat(dto): add constructors and getters/setters to UserUpdateDTO

--- src/main/java/com/nick/job_application_tracker/dto/UserUpdateDTO.java
+++ src/main/java/com/nick/job_application_tracker/dto/UserUpdateDTO.java
@@ -4,4 +4,43 @@ public class UserUpdateDTO {
     public String email;
     public String password;
     public boolean enabled;
+    
+    public UserUpdateDTO(String email, String password) {
+        this.email = email;
+        this.password = password;
+        this.enabled = true;
+    }
+    
+    public UserUpdateDTO(String email, String password, boolean enabled) {
+        this(email, password);
+        this.enabled = enabled;
+    }
+
+    // getters and setters
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+    
+    
 }
