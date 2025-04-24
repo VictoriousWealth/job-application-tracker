700511f5175f79560348b22a489aff038943ea44
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-24 19:08:15 +0100

    feat(dto): add getters and setters to UserInfoDTO

--- src/main/java/com/nick/job_application_tracker/dto/UserInfoDTO.java
+++ src/main/java/com/nick/job_application_tracker/dto/UserInfoDTO.java
@@ -14,4 +14,36 @@ public class UserInfoDTO {
         this.enabled = enabled;
         this.roles = roles;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
 }
