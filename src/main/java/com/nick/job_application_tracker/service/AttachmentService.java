6e6be915d7f7a81c26cbfbccd4cb72a95d040aec
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-24 20:45:50 +0100

    refactor(service): use AttachmentMapper in AttachmentService

--- src/main/java/com/nick/job_application_tracker/service/AttachmentService.java
+++ src/main/java/com/nick/job_application_tracker/service/AttachmentService.java
@@ -5,48 +5,33 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.nick.job_application_tracker.dto.AttachmentDTO;
+import com.nick.job_application_tracker.mapper.AttachmentMapper;
 import com.nick.job_application_tracker.model.Attachment;
-import com.nick.job_application_tracker.model.JobApplication;
 import com.nick.job_application_tracker.repository.AttachmentRepository;
 
 @Service
 public class AttachmentService {
 
     private final AttachmentRepository repository;
+    private final AttachmentMapper mapper;
 
-    public AttachmentService(AttachmentRepository repo) {
-        this.repository = repo;
+    public AttachmentService(AttachmentRepository repository, AttachmentMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
     }
 
     public List<AttachmentDTO> getByJobAppId(Long jobAppId) {
         return repository.findByJobApplicationId(jobAppId).stream()
-            .map(this::toDTO)
+            .map(mapper::toDTO)
             .toList();
     }
 
     public AttachmentDTO save(AttachmentDTO dto) {
-        Attachment attachment = new Attachment();
-        attachment.setType(Attachment.Type.valueOf(dto.type));
-        attachment.setFilePath(dto.filePath);
-
-        JobApplication job = new JobApplication();
-        job.setId(dto.jobApplicationId);
-        attachment.setJobApplication(job);
-
-        return toDTO(repository.save(attachment));
+        Attachment attachment = mapper.toEntity(dto);
+        return mapper.toDTO(repository.save(attachment));
     }
 
     public void delete(Long id) {
         repository.deleteById(id);
     }
-
-    private AttachmentDTO toDTO(Attachment attachment) {
-        return new AttachmentDTO(
-            attachment.getId(),
-            attachment.getType().name(),
-            attachment.getFilePath(),
-            attachment.getJobApplication().getId()
-        );
-    }
-
 }
