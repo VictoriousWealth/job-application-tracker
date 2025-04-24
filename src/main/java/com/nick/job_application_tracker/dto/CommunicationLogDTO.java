0ec2bbc7d0841c63292cb59b34dd70b1eb69d0c5
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-24 19:08:15 +0100

    feat(dto): add getters and setters to CommunicationLogDTO

--- src/main/java/com/nick/job_application_tracker/dto/CommunicationLogDTO.java
+++ src/main/java/com/nick/job_application_tracker/dto/CommunicationLogDTO.java
@@ -20,4 +20,52 @@ public class CommunicationLogDTO {
         this.message = message;
         this.jobApplicationId = jobApplicationId;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Long getJobApplicationId() {
+        return jobApplicationId;
+    }
+
+    public void setJobApplicationId(Long jobApplicationId) {
+        this.jobApplicationId = jobApplicationId;
+    }
 }
