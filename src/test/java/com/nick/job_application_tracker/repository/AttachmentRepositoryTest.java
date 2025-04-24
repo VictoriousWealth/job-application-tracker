9ad93714f24b18949c8e39b8e29feeb6577d3c46
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-24 18:22:40 +0100

    test(repository): add @Transactional to AttachmentRepositoryTest

--- src/test/java/com/nick/job_application_tracker/repository/AttachmentRepositoryTest.java
+++ src/test/java/com/nick/job_application_tracker/repository/AttachmentRepositoryTest.java
@@ -14,7 +14,11 @@ import com.nick.job_application_tracker.model.JobApplication;
 import com.nick.job_application_tracker.model.Role;
 import com.nick.job_application_tracker.model.User;
 
+import jakarta.transaction.Transactional;
+
+
 @SpringBootTest
+@Transactional
 @ActiveProfiles("test")
 public class AttachmentRepositoryTest {
 
