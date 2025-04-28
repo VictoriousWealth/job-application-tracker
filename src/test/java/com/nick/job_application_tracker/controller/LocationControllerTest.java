867a673a53b3cbe16064048723296227bd901e24
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-28 11:35:11 +0100

    test(controller): add unit tests for LocationController
    This commit introduces a new test class, LocationControllerTest, to validate
    the functionality of the LocationController. The test checks that a GET
    request to the controller's endpoint returns a 200 OK status. This addition
    improves our test coverage and ensures that the controller behaves as
    expected, reducing the risk of future regressions in this area.

---
+++ src/test/java/com/nick/job_application_tracker/controller/LocationControllerTest.java
@@ -0,0 +1,21 @@
+package com.nick.job_application_tracker.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+class LocationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testController() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/LocationController"))
+               .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
