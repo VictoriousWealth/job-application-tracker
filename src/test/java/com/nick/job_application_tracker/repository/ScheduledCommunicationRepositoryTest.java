3ddbda14947189666593b20c24aa2f16aa0183ff
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-28 07:51:14 +0100

    test: use TestPropertySource for ScheduledCommunicationRepositoryTest

--- src/test/java/com/nick/job_application_tracker/repository/ScheduledCommunicationRepositoryTest.java
+++ src/test/java/com/nick/job_application_tracker/repository/ScheduledCommunicationRepositoryTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import com.nick.job_application_tracker.model.JobApplication;
 import com.nick.job_application_tracker.model.JobApplication.Status;
@@ -22,7 +22,7 @@ import jakarta.transaction.Transactional;
 
 @SpringBootTest
 @Transactional
-@ActiveProfiles("test")
+@TestPropertySource("classpath:application-test.properties") 
 public class ScheduledCommunicationRepositoryTest {
 
     @Autowired
