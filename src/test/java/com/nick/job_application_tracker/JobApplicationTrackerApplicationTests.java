2cadf1081adffc3f6126c07b9d5ed18c0f4c58a5
Nick Efe Oni <neoni1@sheffield.ac.uk>
2025-04-28 07:51:14 +0100

    test: remove ActiveProfiles and add test for database connection

--- src/test/java/com/nick/job_application_tracker/JobApplicationTrackerApplicationTests.java
+++ src/test/java/com/nick/job_application_tracker/JobApplicationTrackerApplicationTests.java
@@ -1,15 +1,22 @@
 package com.nick.job_application_tracker;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
+// @ActiveProfiles("test")
 @SpringBootTest
 class JobApplicationTrackerApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+	@Value("${spring.datasource.url}")
+	private String datasourceUrl;
 
+	@Test
+	void testDatabaseConnection() {
+		System.out.println("Datasource URL: " + datasourceUrl);
+		// Your test logic here
+	}
 }
