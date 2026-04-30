afc9577f0e70fcf451ecd158a76dda9dabf5f7cd
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-04-30 14:13:22 +0100

    feat(dto): add id field to JobSourceResponseDTO
    This update introduces a new id field to the JobSourceResponseDTO class.
    
    The id field is of type UUID, allowing for unique identification of job sources.

--- src/main/java/com/nick/job_application_tracker/dto/response/JobSourceResponseDTO.java
+++ src/main/java/com/nick/job_application_tracker/dto/response/JobSourceResponseDTO.java
@@ -2,9 +2,18 @@ package com.nick.job_application_tracker.dto.response;
 
 public class JobSourceResponseDTO {
 
+    private java.util.UUID id;
     private String name;
 
     // --- Getters and Setters ---
+
+    public java.util.UUID getId() {
+        return id;
+    }
+
+    public void setId(java.util.UUID id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
