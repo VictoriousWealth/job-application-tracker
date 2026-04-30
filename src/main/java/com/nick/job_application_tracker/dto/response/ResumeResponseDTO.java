a87f461ad083b3ebf839ca5347c64f9fb8de3022
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-04-30 14:13:27 +0100

    feat(dto): add id field to ResumeResponseDTO
    This update introduces a new UUID field to the ResumeResponseDTO.
    
    The id field allows for unique identification of resume responses.
    
    Getters and setters for the id field have also been added.

--- src/main/java/com/nick/job_application_tracker/dto/response/ResumeResponseDTO.java
+++ src/main/java/com/nick/job_application_tracker/dto/response/ResumeResponseDTO.java
@@ -4,10 +4,19 @@ import java.util.UUID;
 
 public class ResumeResponseDTO {
 
+    private UUID id;
     private String title;
     private String filePath;
 
     // --- Getters and Setters ---
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
