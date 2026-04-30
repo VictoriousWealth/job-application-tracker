94a1129930f5602f837c0644984a5c2d7c284ea6
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-04-30 13:45:06 +0100

    feat(model): add admin role
    This update introduces a new ADMIN role to the Role enum.
    
    The addition allows for more granular role management in the application.

--- src/main/java/com/nick/job_application_tracker/model/Role.java
+++ src/main/java/com/nick/job_application_tracker/model/Role.java
@@ -1,7 +1,8 @@
 package com.nick.job_application_tracker.model;
 
 public enum Role {
-    BASIC;
+    BASIC,
+    ADMIN;
 
     public static Role from(String value) {
         return Role.valueOf(value.toUpperCase());
