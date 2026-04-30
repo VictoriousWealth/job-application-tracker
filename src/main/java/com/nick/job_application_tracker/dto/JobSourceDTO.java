11734c19debb9357c6551c016758c033648ee222
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-04-30 14:12:50 +0100

    refactor(dto): remove legacy id handling
    The constructor and setter for id using Long have been removed.
    
    This change simplifies the JobSourceDTO class by eliminating unnecessary legacy code.

--- src/main/java/com/nick/job_application_tracker/dto/JobSourceDTO.java
+++ src/main/java/com/nick/job_application_tracker/dto/JobSourceDTO.java
@@ -13,20 +13,12 @@ public class JobSourceDTO {
         this.name = name;
     }
 
-    public JobSourceDTO(Long id, String name) {
-        this(LegacyIdAdapter.fromLong(id), name);
-    }
-
     public UUID getId() {
         return id;
     }
 
     public void setId(UUID id) {
         this.id = id;
-    }
-
-    public void setId(Long id) {
-        this.id = LegacyIdAdapter.fromLong(id);
     }
 
     public String getName() {
