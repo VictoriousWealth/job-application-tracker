afe634d62f69daf68d3b37d88637dacaf0fbfbe5
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-04-30 14:14:06 +0100

    refactor(repository): remove legacy id adapter methods
    Removed default methods for findById and deleteById that used LegacyIdAdapter.
    
    This change simplifies the interface and removes unnecessary complexity.

--- src/main/java/com/nick/job_application_tracker/repository/inter_face/LocationRepository.java
+++ src/main/java/com/nick/job_application_tracker/repository/inter_face/LocationRepository.java
@@ -1,6 +1,5 @@
 package com.nick.job_application_tracker.repository.inter_face;
 
-import com.nick.job_application_tracker.dto.LegacyIdAdapter;
 import com.nick.job_application_tracker.model.Location;
 import com.nick.job_application_tracker.repository.custom.LocationRepositoryCustom;
 
@@ -12,14 +11,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface LocationRepository extends JpaRepository<Location, UUID>, LocationRepositoryCustom {
-    default Optional<Location> findById(Long id) {
-        return findById(LegacyIdAdapter.fromLong(id));
-    }
-
-    default void deleteById(Long id) {
-        deleteById(LegacyIdAdapter.fromLong(id));
-    }
-
     Optional<Location> findByCityAndCountry(String city, String country);
     Page<Location> findAllByDeletedFalse(Pageable pageable);
 }
