d3d044cf43d25a19bd3f4cf44fbb793d3beeaabf
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-04-30 09:30:28 +0100

    feat(mapper): enhance ResumeMapper to include title
    Updated the toDTO method to map the title from Resume.
    
    Modified the toEntity method to set a default title if none is provided.

--- src/main/java/com/nick/job_application_tracker/mapper/ResumeMapper.java
+++ src/main/java/com/nick/job_application_tracker/mapper/ResumeMapper.java
@@ -8,11 +8,12 @@ import com.nick.job_application_tracker.model.Resume;
 @Component
 public class ResumeMapper {
     public static ResumeDTO toDTO(Resume resume) {
-        return new ResumeDTO(resume.getId(), resume.getFilePath());
+        return new ResumeDTO(resume.getId(), resume.getTitle(), resume.getFilePath());
     }
 
     public static Resume toEntity(ResumeDTO dto) {
         Resume resume = new Resume();
+        resume.setTitle(dto.getTitle() != null ? dto.getTitle() : dto.getFilePath());
         resume.setFilePath(dto.getFilePath());
         return resume;
     }
