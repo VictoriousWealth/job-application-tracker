ff73e61ee84f53278252fdd28fe99ea3cf7ea67e
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-04-30 14:14:41 +0100

    feat(service): add SkillTrackerService implementation
    This commit introduces the SkillTrackerService class.
    
    It provides methods to create, retrieve, and delete skill trackers associated with job applications.
    
    The service integrates with repositories and includes auditing for create and delete operations.

---
+++ src/main/java/com/nick/job_application_tracker/service/implementation/SkillTrackerService.java
@@ -0,0 +1,55 @@
+package com.nick.job_application_tracker.service.implementation;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.nick.job_application_tracker.dto.create.SkillTrackerCreateDTO;
+import com.nick.job_application_tracker.dto.response.SkillTrackerResponseDTO;
+import com.nick.job_application_tracker.mapper.SkillTrackerMapper;
+import com.nick.job_application_tracker.model.JobApplication;
+import com.nick.job_application_tracker.model.SkillTracker;
+import com.nick.job_application_tracker.repository.inter_face.JobApplicationRepository;
+import com.nick.job_application_tracker.repository.inter_face.SkillTrackerRepository;
+import com.nick.job_application_tracker.service.inter_face.AuditLogService;
+import com.nick.job_application_tracker.service.specialised_common.JobApplicationServiceInterface;
+
+@Service
+public class SkillTrackerService {
+
+    private final SkillTrackerRepository repo;
+    private final JobApplicationServiceInterface jobApplicationService;
+    private final AuditLogService auditLogService;
+
+    public SkillTrackerService(
+        SkillTrackerRepository repo,
+        JobApplicationServiceInterface jobApplicationService,
+        AuditLogService auditLogService
+    ) {
+        this.repo = repo;
+        this.jobApplicationService = jobApplicationService;
+        this.auditLogService = auditLogService;
+    }
+
+    public List<SkillTrackerResponseDTO> getByJobAppId(UUID jobAppId) {
+        return repo.findByJobApplicationIdAndDeletedFalse(jobAppId, Pageable.unpaged()).getContent().stream()
+            .map(SkillTrackerMapper::toResponseDTO)
+            .toList();
+    }
+
+    public SkillTrackerResponseDTO create(SkillTrackerCreateDTO dto) {
+        JobApplication jobApplication = jobApplicationService.getModelById(dto.getJobApplicationId());
+        SkillTracker skill = SkillTrackerMapper.toEntity(dto, jobApplication);
+        SkillTracker savedSkill = repo.save(skill);
+        auditLogService.logCreate("Created SkillTracker with ID " + savedSkill.getId() + " for JobApplication ID " + savedSkill.getJobApplication().getId());
+        return SkillTrackerMapper.toResponseDTO(savedSkill);
+    }
+
+    public void delete(UUID id) {
+        repo.deleteById(id);
+        auditLogService.logDelete("Deleted SkillTracker with ID " + id);
+    }
+
+}
