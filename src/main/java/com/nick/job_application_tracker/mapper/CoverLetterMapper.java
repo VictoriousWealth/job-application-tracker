65af8913f8f9a9697d70c3ee0d80827dc6dda058
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-04-30 14:13:35 +0100

    feat(mapper): add cover letter mapping methods
    Introduced new methods to map CoverLetter entities to various DTOs.
    
    These include methods for creating, updating, and retrieving cover letter details.
    
    This enhances the functionality of the CoverLetterMapper class.

--- src/main/java/com/nick/job_application_tracker/mapper/CoverLetterMapper.java
+++ src/main/java/com/nick/job_application_tracker/mapper/CoverLetterMapper.java
@@ -3,10 +3,47 @@ package com.nick.job_application_tracker.mapper;
 import org.springframework.stereotype.Component;
 
 import com.nick.job_application_tracker.dto.CoverLetterDTO;
+import com.nick.job_application_tracker.dto.create.CoverLetterCreateDTO;
+import com.nick.job_application_tracker.dto.detail.CoverLetterDetailDTO;
+import com.nick.job_application_tracker.dto.response.CoverLetterResponseDTO;
+import com.nick.job_application_tracker.dto.update.CoverLetterUpdateDTO;
 import com.nick.job_application_tracker.model.CoverLetter;
 
 @Component
 public class CoverLetterMapper {
+
+    public CoverLetterResponseDTO toResponseDTO(CoverLetter entity) {
+        CoverLetterResponseDTO dto = new CoverLetterResponseDTO();
+        dto.setId(entity.getId());
+        dto.setTitle(entity.getTitle());
+        dto.setFilePath(entity.getFilePath());
+        dto.setContent(entity.getContent());
+        return dto;
+    }
+
+    public CoverLetterDetailDTO toDetailDTO(CoverLetter entity) {
+        CoverLetterDetailDTO dto = new CoverLetterDetailDTO();
+        dto.setId(entity.getId());
+        dto.setTitle(entity.getTitle());
+        dto.setFilePath(entity.getFilePath());
+        dto.setContent(entity.getContent());
+        return dto;
+    }
+
+    public CoverLetter toEntity(CoverLetterCreateDTO dto) {
+        CoverLetter coverLetter = new CoverLetter();
+        coverLetter.setTitle(dto.getTitle());
+        coverLetter.setFilePath(dto.getFilePath());
+        coverLetter.setContent(dto.getContent());
+        return coverLetter;
+    }
+
+    public CoverLetter updateEntityWithDTOInfo(CoverLetter coverLetter, CoverLetterUpdateDTO dto) {
+        coverLetter.setTitle(dto.getTitle());
+        coverLetter.setFilePath(dto.getFilePath());
+        coverLetter.setContent(dto.getContent());
+        return coverLetter;
+    }
 
     public CoverLetterDTO toDTO(CoverLetter entity) {
         return new CoverLetterDTO(
