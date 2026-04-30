a4d9f90668a71b2cb6365f1cc523e388ad890464
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-04-30 15:26:38 +0100

    refactor(repository): remove custom repository interface
    The JobSourceRepository no longer extends JobSourceRepositoryCustom.
    
    This change simplifies the repository interface and focuses on standard JPA functionality.
    
    Additionally, a new method to find JobSource by ID while checking for deletion status has been added.

--- src/main/java/com/nick/job_application_tracker/repository/inter_face/JobSourceRepository.java
+++ src/main/java/com/nick/job_application_tracker/repository/inter_face/JobSourceRepository.java
@@ -1,7 +1,6 @@
 package com.nick.job_application_tracker.repository.inter_face;
 
 import com.nick.job_application_tracker.model.JobSource;
-import com.nick.job_application_tracker.repository.custom.JobSourceRepositoryCustom;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,7 +11,9 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface JobSourceRepository extends JpaRepository<JobSource, UUID>, JobSourceRepositoryCustom {
+public interface JobSourceRepository extends JpaRepository<JobSource, UUID> {
+
+    Optional<JobSource> findByIdAndDeletedFalse(UUID id);
 
     Optional<JobSource> findByNameIgnoreCaseAndDeletedFalse(String name);
 
