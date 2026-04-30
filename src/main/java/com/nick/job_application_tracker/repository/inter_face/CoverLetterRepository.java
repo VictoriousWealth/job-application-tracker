d9bdc70fc22760beef3777dac0e75ef8fb1a0529
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-04-30 15:26:31 +0100

    refactor(repository): update cover letter repository methods
    Refactored the CoverLetterRepository interface to improve method signatures.
    
    Changed methods to use 'createdBy' as a parameter instead of 'userId' for better clarity.
    
    Added new methods to handle cover letters based on their draft status and deletion state.

--- src/main/java/com/nick/job_application_tracker/repository/inter_face/CoverLetterRepository.java
+++ src/main/java/com/nick/job_application_tracker/repository/inter_face/CoverLetterRepository.java
@@ -1,23 +1,25 @@
 package com.nick.job_application_tracker.repository.inter_face;
 
 import com.nick.job_application_tracker.model.CoverLetter;
-import com.nick.job_application_tracker.repository.custom.CoverLetterRepositoryCustom;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface CoverLetterRepository extends JpaRepository<CoverLetter, UUID>, CoverLetterRepositoryCustom {
+public interface CoverLetterRepository extends JpaRepository<CoverLetter, UUID> {
 
-    Page<CoverLetter> findByCreatedByIdAndDeletedFalse(UUID userId, Pageable pageable);
+    Optional<CoverLetter> findByIdAndCreatedByAndDeletedFalse(UUID id, String createdBy);
 
-    Page<CoverLetter> findByCreatedByIdAndIsDraftTrueAndDeletedFalse(UUID userId, Pageable pageable);
+    Page<CoverLetter> findByCreatedByAndDeletedFalse(String createdBy, Pageable pageable);
 
-    Page<CoverLetter> findByCreatedByIdAndDeletedTrue(UUID userId, Pageable pageable);
+    Page<CoverLetter> findByCreatedByAndIsDraftTrueAndDeletedFalse(String createdBy, Pageable pageable);
 
-    long countByCreatedByIdAndDeletedFalse(UUID userId);
+    Page<CoverLetter> findByCreatedByAndDeletedTrue(String createdBy, Pageable pageable);
+
+    long countByCreatedByAndDeletedFalse(String createdBy);
 }
