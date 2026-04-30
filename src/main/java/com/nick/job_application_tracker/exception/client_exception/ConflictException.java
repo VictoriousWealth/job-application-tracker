acc28a6b0a3178bd921c1c3311d2bb9d9adf6bbc
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-04-30 09:30:18 +0100

    fix(exception): update ConflictException constructor
    Changed the ConflictException constructor to pass null as the second argument.
    
    This change aligns the constructor with the expected parameters for ClientException.

--- src/main/java/com/nick/job_application_tracker/exception/client_exception/ConflictException.java
+++ src/main/java/com/nick/job_application_tracker/exception/client_exception/ConflictException.java
@@ -2,6 +2,6 @@ package com.nick.job_application_tracker.exception.client_exception;
 
 public class ConflictException extends ClientException {
     public ConflictException(String message) {
-        super(message);
+        super(message, null);
     }
 }
