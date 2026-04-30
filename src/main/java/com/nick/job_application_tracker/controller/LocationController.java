974a300bdbe2f7bf59a7c400bf60eaee80750252
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-04-30 09:29:38 +0100

    feat(controller): update location management methods
    Refactored the LocationController to improve location management.
    
    Added methods for creating, retrieving, updating, and deleting locations.
    
    Updated API documentation and removed unused imports.

--- src/main/java/com/nick/job_application_tracker/controller/LocationController.java
+++ src/main/java/com/nick/job_application_tracker/controller/LocationController.java
@@ -3,11 +3,9 @@ package com.nick.job_application_tracker.controller;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -15,16 +13,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.nick.job_application_tracker.dto.create.LocationCreateDTO;
-import com.nick.job_application_tracker.dto.response.LocationResponseDTO;
+import com.nick.job_application_tracker.dto.LocationDTO;
 import com.nick.job_application_tracker.service.implementation.LocationService;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
@@ -39,128 +31,38 @@ public class LocationController {
         this.locationService = locationService;
     }
 
-
-
-    // ################################################################
-    // CREATE
-    // ################################################################
-
-    @Operation(summary = "Create a new location")
-    @ApiResponses({
-        @ApiResponse(responseCode = "201", description = "Location created successfully",
-            content = @Content(schema = @Schema(implementation = LocationResponseDTO.class))),
-        @ApiResponse(responseCode = "400", description = "Invalid input"),
-        @ApiResponse(responseCode = "500", description = "Server error")
-    })
-    /*
-     * This method allows the creation of a job application trhoguh 
-     * the use of a JobApplicationCreateDTO and returns a 
-     * JobApplicationResponseDTO
-     */
-    @PostMapping
-    public ResponseEntity<LocationResponseDTO> create(@Valid @RequestBody LocationCreateDTO dto) {
-        return ResponseEntity.status(201).body(locationService.create(dto));
-    }
-
-
-
-    // ################################################################
-    // READ
-    // ################################################################
-
-    @Operation(summary = "Get detailed information for a specific location")
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "Detailed location info",
-            content = @Content(schema = @Schema(implementation = JobApplicationDetailDTO.class))),
-        @ApiResponse(responseCode = "404", description = "Location not found"),
-        @ApiResponse(responseCode = "400", description = "Invalid input"),
-        @ApiResponse(responseCode = "500", description = "Server error")
-    })
-    /*
-     * This method acepts a uuid id and retrieves a job application details 
-     * in the form of a JobApplicationDetailDTO
-     */
-    @GetMapping("/{id}")
-    public ResponseEntity<JobApplicationDetailDTO> getById(@PathVariable UUID id) {
-        return ResponseEntity.status(200).body(jobApplicationService.getById(id));
-    }
-
-
-    @Operation(summary = "Get all job applications for the current user")
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "List of job applications",
-            content = @Content(schema = @Schema(implementation = JobApplicationResponseDTO.class))),
-        @ApiResponse(responseCode = "500", description = "Server error")
-    })
-    /**
-     * 
-     * @return a list of JobApplicationResponseDTO
-     */
+    @Operation(summary = "Get all locations")
     @GetMapping
-    public ResponseEntity<Page<JobApplicationResponseDTO>> getAll(Pageable pageable) {
-        return ResponseEntity.ok(jobApplicationService.getAll(pageable));
+    public ResponseEntity<List<LocationDTO>> getAll() {
+        return ResponseEntity.ok(locationService.getAllLocations());
     }
 
-
-
-    // ################################################################
-    // UPDATE
-    // ################################################################
-
-    @Operation(summary = "Patch a set of all details of a job application for the current user")
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "Job application has been successfully patched",
-            content = @Content(schema = @Schema(implementation = JobApplicationResponseDTO.class))),
-        @ApiResponse(responseCode = "400", description = "Invalid input"),
-        @ApiResponse(responseCode = "404", description = "Job application not found"),
-        @ApiResponse(responseCode = "500", description = "Server error")
-    })
-    /**
-     * 
-     * @return a JobApplicationDetailDTO
-     */
-    @PatchMapping("/{id}")
-    public ResponseEntity<JobApplicationDetailDTO> patchById(@PathVariable UUID id, @RequestBody JsonNode node) {
-        return ResponseEntity.ok(jobApplicationService.patchById(id, node));
+    @Operation(summary = "Get a location by ID")
+    @GetMapping("/{id}")
+    public ResponseEntity<LocationDTO> getById(@PathVariable UUID id) {
+        return locationService.getLocationById(id)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
     }
 
-    @Operation(summary = "Update all details of a job application for the current user")
-    @ApiResponses({
-        @ApiResponse(responseCode = "200", description = "Job application has been successfully updated",
-            content = @Content(schema = @Schema(implementation = JobApplicationResponseDTO.class))),
-        @ApiResponse(responseCode = "400", description = "Invalid input"),
-        @ApiResponse(responseCode = "404", description = "Job application not found"),
-        @ApiResponse(responseCode = "500", description = "Server error")
-    })
-    /**
-     * 
-     * @return a JobApplicationDetailDTO
-     */
+    @Operation(summary = "Create a location")
+    @PostMapping
+    public ResponseEntity<LocationDTO> create(@Valid @RequestBody LocationDTO dto) {
+        return ResponseEntity.ok(locationService.createLocation(dto));
+    }
+
+    @Operation(summary = "Update a location")
     @PutMapping("/{id}")
-    public ResponseEntity<JobApplicationDetailDTO> updateById(@PathVariable UUID id, @RequestBody JobApplicationUpdateDTO dto) {
-        return ResponseEntity.ok(jobApplicationService.update(id, dto));
+    public ResponseEntity<LocationDTO> update(@PathVariable UUID id, @Valid @RequestBody LocationDTO dto) {
+        return locationService.updateLocation(id, dto)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
     }
 
-
-
-    // ################################################################
-    // DELETE
-    // ################################################################
-
-    @Operation(summary = "Patch a set of all details of a job application for the current user")
-    @ApiResponses({
-        @ApiResponse(responseCode = "204", description = "Job application has been soft-deleted",
-            content = @Content(schema = @Schema(implementation = JobApplicationResponseDTO.class))),        @ApiResponse(responseCode = "404", description = "Job application not found"),
-        @ApiResponse(responseCode = "400", description = "Invalid input"),
-        @ApiResponse(responseCode = "500", description = "Server error")
-    })
-    /**
-     * 
-     * @return No content response
-     */
+    @Operation(summary = "Delete a location")
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteById(@PathVariable UUID id) {
-        return ResponseEntity.status(204).body(jobApplicationService.deleteById(id));
+    public ResponseEntity<Void> delete(@PathVariable UUID id) {
+        locationService.deleteLocation(id);
+        return ResponseEntity.noContent().build();
     }
-
 }
