c0bc58e3e77cd628ebe17859aeecf517afcb5721
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-05-01 13:48:22 +0100

    fix(service): update import for NotFoundException
    Changed the import path for NotFoundException.
    
    This aligns with the new package structure for exceptions.

--- src/main/java/com/nick/job_application_tracker/service/implementation/FollowUpReminderService.java
+++ src/main/java/com/nick/job_application_tracker/service/implementation/FollowUpReminderService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.nick.job_application_tracker.dto.create.FollowUpReminderCreateDTO;
 import com.nick.job_application_tracker.dto.response.FollowUpReminderResponseDTO;
-import com.nick.job_application_tracker.exception.client_exception.NotFoundException;
+import com.nick.job_application_tracker.exception.client.NotFoundException;
 import com.nick.job_application_tracker.mapper.FollowUpReminderMapper;
 import com.nick.job_application_tracker.model.FollowUpReminder;
 import com.nick.job_application_tracker.model.JobApplication;
