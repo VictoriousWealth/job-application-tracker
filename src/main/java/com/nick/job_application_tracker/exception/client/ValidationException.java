6ddd7088f44cf08651ab11ca29a7bb9da01987b0
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-05-01 13:47:43 +0100

    feat(exception): add ValidationException class
    This commit introduces a new ValidationException class.
    
    It extends ClientException and provides a constructor for message and cause.

---
+++ src/main/java/com/nick/job_application_tracker/exception/client/ValidationException.java
@@ -0,0 +1,7 @@
+package com.nick.job_application_tracker.exception.client;
+
+public class ValidationException extends ClientException {
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
