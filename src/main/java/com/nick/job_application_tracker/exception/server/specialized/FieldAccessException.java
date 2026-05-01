6c2805d923a3e0a0b8477358950a3622377311a8
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-05-01 13:48:05 +0100

    feat(exception): add FieldAccessException for field access errors
    This commit introduces a new exception class for handling cases
    
    where a field cannot be accessed or modified during dynamic operations.
    
    It extends InternalServerErrorException to provide a specific error context.

---
+++ src/main/java/com/nick/job_application_tracker/exception/server/specialized/FieldAccessException.java
@@ -0,0 +1,12 @@
+package com.nick.job_application_tracker.exception.server.specialized;
+
+import com.nick.job_application_tracker.exception.server.InternalServerErrorException;
+
+/**
+ * Thrown when a field cannot be accessed/reflected during dynamic operations.
+ */
+public class FieldAccessException extends InternalServerErrorException {
+    public FieldAccessException(String fieldName, Throwable cause) {
+        super("Unable to access or modify field: " + fieldName, cause);
+    }
+}
