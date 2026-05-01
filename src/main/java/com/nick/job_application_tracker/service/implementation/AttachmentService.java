f328c76168700b0b93ab8074cf13bb13383ed168
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-05-01 13:31:48 +0100

    fix(service): correct interface naming in AttachmentService
    Updated import statements to fix interface naming conventions.
    
    This change ensures consistency in the codebase and adheres to the correct package structure.

--- src/main/java/com/nick/job_application_tracker/service/implementation/AttachmentService.java
+++ src/main/java/com/nick/job_application_tracker/service/implementation/AttachmentService.java
@@ -13,10 +13,10 @@ import com.nick.job_application_tracker.mapper.AttachmentMapper;
 import com.nick.job_application_tracker.model.Attachment;
 import com.nick.job_application_tracker.model.JobApplication;
 import com.nick.job_application_tracker.model.User;
-import com.nick.job_application_tracker.repository.inter_face.AttachmentRepository;
-import com.nick.job_application_tracker.repository.inter_face.JobApplicationRepository;
-import com.nick.job_application_tracker.repository.inter_face.UserRepository;
-import com.nick.job_application_tracker.service.inter_face.AuditLogService;
+import com.nick.job_application_tracker.repository.interfaces.AttachmentRepository;
+import com.nick.job_application_tracker.repository.interfaces.JobApplicationRepository;
+import com.nick.job_application_tracker.repository.interfaces.UserRepository;
+import com.nick.job_application_tracker.service.interfaces.AuditLogService;
 import com.nick.job_application_tracker.util.SecurityUtils;
 
 @Service
