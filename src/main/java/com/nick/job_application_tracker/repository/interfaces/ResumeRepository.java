44c49bb42d66feafd6c5665cc68968199678ff54
Nick Efe Oni <neoni1@sheffield.ac.uk>
2026-05-10 02:14:36 +0100

    feat(repository): add reassignOwnership method
    Introduced a new method to update the ownership of resumes.
    
    This allows for reassignment of the createdBy field, enhancing the flexibility of resume management.

--- src/main/java/com/nick/job_application_tracker/repository/interfaces/ResumeRepository.java
+++ src/main/java/com/nick/job_application_tracker/repository/interfaces/ResumeRepository.java
@@ -5,6 +5,9 @@ import com.nick.job_application_tracker.model.Resume;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -26,4 +29,8 @@ public interface ResumeRepository extends JpaRepository<Resume, UUID> {
     long countByCreatedByAndDeletedFalse(String createdBy);
 
     long countByDeletedFalse();
+
+    @Modifying
+    @Query("update Resume r set r.createdBy = :newCreatedBy where r.createdBy = :oldCreatedBy")
+    int reassignOwnership(@Param("oldCreatedBy") String oldCreatedBy, @Param("newCreatedBy") String newCreatedBy);
 }
